b6d54cf7634ab28cebe7123c9bd1e496adc5bf56
Alzhanbaev Kamchybek <aljanbaevkam@gmail.com>
2023-12-12 23:48:54 +0600

    Added UserServiceImpl CRUD: save, findById, findAll, delete. Added UserController (saveBasket, findById, findAll, update, delete). Tested in Swagger.

---
+++ mMar/src/main/java/com/kg/mmar/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.kg.mmar.controller;
+
+import com.kg.mmar.dto.UserDto;
+import com.kg.mmar.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    @PostMapping("save")
+    public UserDto saveUser(@RequestBody UserDto userDto) {
+        return userService.saveUser(userDto);
+    }
+
+    @GetMapping("/findById")
+    public UserDto findById(@RequestParam Long id) {
+        return userService.findByIdUser(id);
+    }
+
+    @GetMapping("findAll")
+    public List<UserDto> findAll() {
+        return userService.findAllUser();
+    }
+
+    @PutMapping("/update")
+    public UserDto updateUser(@RequestBody UserDto userDto) {
+        return userService.updateUser(userDto);
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@RequestParam Long id) {
+        userService.deleteUser(id);
+    }
+
+}
--- mMar/src/main/java/com/kg/mmar/service/impl/UserServiceImpl.java
+++ mMar/src/main/java/com/kg/mmar/service/impl/UserServiceImpl.java
@@ -1,33 +1,53 @@
 package com.kg.mmar.service.impl;
 
 import com.kg.mmar.dto.UserDto;
+import com.kg.mmar.entity.User;
+import com.kg.mmar.mapper.UserMapper;
+import com.kg.mmar.repository.UserRepo;
 import com.kg.mmar.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+@Transactional
+
 public class UserServiceImpl implements UserService {
+    private final UserRepo userRepo;
+    private final UserMapper userMapper;
     @Override
     public UserDto saveUser(UserDto userDto) {
-        return null;
+        User user = userMapper.toEntity(userDto);
+        User save = userRepo.save(user);
+        return userMapper.toDto(save);
     }
 
     @Override
     public UserDto findByIdUser(Long id) {
-        return null;
+        return userMapper.toDto(userRepo.findById(id).get());
     }
 
     @Override
     public List<UserDto> findAllUser() {
-        return null;
+        return userMapper.toDtoList(userRepo.findAll());
     }
 
     @Override
     public UserDto updateUser(UserDto userDto) {
-        return null;
+        User updateUserUr = userRepo.findById(userDto.getId()).get();
+        updateUserUr.setFirstName(userDto.getFirstName());
+        updateUserUr.setLastName(userDto.getLastName());
+        updateUserUr.setEmail(userDto.getEmail());
+        return userMapper.toDto(updateUserUr);
     }
 
     @Override
     public void deleteUser(Long id) {
+        User deleteUser = userRepo.findById(id).get();
+        userRepo.delete(deleteUser);
 
     }
 }
